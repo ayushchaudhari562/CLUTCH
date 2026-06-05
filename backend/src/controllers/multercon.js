6d3e55137ffbf27ba058549c20260f3ffa9f4f37
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-06-06 00:34:59 +0530

    'Auto-commit'

--- backend/src/controllers/multercon.js
+++ backend/src/controllers/multercon.js
@@ -40,7 +40,7 @@ const getAllPosts = async (req,res)=>{
         //me aur pdhana hai for now i only know that its 
         // make easier to without querey
         //..
-        const posts = await
+        const posts = await prisma
     }
 }
 
