e1146d26446ccd4197c674b378456da549a094bd
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-06-06 02:07:14 +0530

    'Auto-commit'

--- clutch-client/src/pages/Campus-Feed.jsx
+++ clutch-client/src/pages/Campus-Feed.jsx
@@ -165,7 +165,9 @@ const CampusFeed = () => {
           </div>
 
           <div>
-            
+            {posts.map((post) => (
+              <PostCard key={post.id} post={post} />
+            ))}
           </div>
 
         {/* RIGHT COLUMN */}
