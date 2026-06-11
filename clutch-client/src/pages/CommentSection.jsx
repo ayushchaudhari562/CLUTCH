7b70e92b7125e622f2f613177e4cebe46c1429d9
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-06-12 00:35:40 +0530

    'Auto-commit'

--- clutch-client/src/pages/CommentSection.jsx
+++ clutch-client/src/pages/CommentSection.jsx
@@ -11,6 +11,9 @@ const CommentSection = ()=>{
     return(
         <>
             <div className="flex gap-4">
+                <div className="w-10 h-10 rounded-full bg-indigo-900/50 flex items-center justify-center text-indigo-300 shrink-0">
+        U
+      </div>
 
             </div>
         </>
