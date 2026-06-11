c6f9b7c826182bad707f7f8e4d331d174c43d9e1
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-06-12 00:34:46 +0530

    'Auto-commit'

--- clutch-client/src/pages/CommentSection.jsx
+++ clutch-client/src/pages/CommentSection.jsx
@@ -7,7 +7,7 @@ import {useParams,useNavigate} from 'react-router-dom';
 //..
 //..
 const CommentSection = ()=>{
-    const [show]
+    const [showReplies]
     return(
         <>
             hello
