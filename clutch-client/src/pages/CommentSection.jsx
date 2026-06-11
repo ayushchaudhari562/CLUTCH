13e5fc936a2002d0841b2e41f9c065bc6424a207
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-06-12 00:57:59 +0530

    Comment section

--- clutch-client/src/pages/CommentSection.jsx
+++ clutch-client/src/pages/CommentSection.jsx
@@ -26,12 +26,32 @@ const CommentSection = ({comment}) => {
                         <button className="text-ts text-gray-400 havor:text-white font-medium">
                             Reply
                         </button>
+                        
+                        {comment.replies && comment.replies.length > 0 && (
+                            <button 
+                                onClick={()=>setShowReplies(!showReplies)}
+                                className="text-xs text-indigo-400 hover:text-indigo-300 font-medium"
+                            >
+                                {showReplies ? 'Hide Replies' : `View ${comment.replies.length} Replies`}
+                            </button>
+                        )}
                     </div>
+                    {/*the main recursion part is here after clicking reply it will call again to this 
+                    so it will age ka add krunga comments acche se */}
+                    {showReplies && comment.replies && (
+                        <div className="border-l-2 border-[#2d2d2d] pl-6 mt-4 space-y-4">
+                            {comment.replies.map(reply =>(
+                                <CommentsSection key={reply.id} comment={reply}/>
+                            ))}
+                        </div>
+                    )}
 
                 </div>
 
             </div>
         </>
     )
-}
+};
+
+
 export default CommentSection;
