aab8196c227d5edc5b0451e2794b69979a2b8d10
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-06-12 00:35:44 +0530

    'Auto-commit'

--- clutch-client/src/pages/CommentSection.jsx
+++ clutch-client/src/pages/CommentSection.jsx
@@ -1,19 +1,19 @@
-import React,{useState,useEffect} from "react";
-import {useParams,useNavigate} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from 'react-router-dom';
 
 //..
 //..
 //..The main concept of the nested recursion;
 //..
 //..
-const CommentSection = ()=>{
-    const [showReplies,setShowReplies] = useState(false);
-    return(
+const CommentSection = () => {
+    const [showReplies, setShowReplies] = useState(false);
+    return (
         <>
             <div className="flex gap-4">
                 <div className="w-10 h-10 rounded-full bg-indigo-900/50 flex items-center justify-center text-indigo-300 shrink-0">
-        U
-      </div>
+                    U
+                </div>
 
             </div>
         </>
