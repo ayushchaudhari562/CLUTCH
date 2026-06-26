0df9e17932b788cb9cc448563e855a2198d349e7
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-06-27 04:13:58 +0530

    next line

--- clutch-client/src/api/CommentsMain.jsx
+++ clutch-client/src/api/CommentsMain.jsx
@@ -184,7 +184,7 @@ const CommentMain = () => {
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
                         </svg>
                       </div>
-                      <span className="text-xs font-semibold truncate">{college.name}</span>
+                      <span className="text-xs font-semibold">{college.name}</span>
                     </div>
                   </li>
                 ))}
