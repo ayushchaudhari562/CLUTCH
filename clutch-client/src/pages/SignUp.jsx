30b2350179aedb93152f23e58e02723af672eca3
ayushchaudhari562 <ayushchaudhry562@gmail.com>
2026-06-28 05:28:37 +0530

    major fix in sync and vc feature

--- clutch-client/src/pages/SignUp.jsx
+++ clutch-client/src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/clerk-react";
 export default function Signup() {
   return (
     <div className="min-h-screen bg-background flex items-center justify-center p-4">
-      <SignUp routing="path" path="/signup" signInUrl="/signin" />
+      <SignUp routing="path" path="/signup" signInUrl="/signin" forceRedirectUrl="/onboarding" />
     </div>
   );
 }
